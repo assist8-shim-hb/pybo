a689b272b7d3b1440ff8cf5034ae8294c2c0e313
shim <shim_hb@assist-8.com>
2023-12-11 15:19:20 +0900

    postgresql 추가

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,3 +4,13 @@ ALLOWED_HOSTS = ['43.200.2.187']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql psycopg2',
+        'NAME': 'pybo',
+        'USER': 'dbmasteruser',
+        'PASSWORD': '!74jHF6(3.y=Xm.i_VQyK}oGA4.7Mh.Z',
+        'HOST': 'ls-3106bb328ade826f65facd499f5a578bdb1df6d6.cnsl2tipetzp.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
